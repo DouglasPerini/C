3d3cf928905e914798209d0a149c76da6f13cbc9
DouglasPerini <94379920+DouglasPerini@users.noreply.github.com>
2021-11-15 11:56:53 -0300

    Add files via upload

---
+++ Algoritmo 5.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main (){
+    //Declarando variáveis 
+    float valor_por_hora, salario;
+    int horas_trabalhadas; 
+
+    //Entradas
+    printf("Informe valor recebido por hora;");
+    scanf("%f", &valor_por_hora);
+
+    printf("Informe horas trabalhadas por mês:");
+    scanf()
+}
